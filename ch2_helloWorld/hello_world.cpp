33b36ccf39bb4fbc96211ca9994df5600b4da71c
Santiago Mondragon <santiago.mondragon224@gmail.com>
2022-02-11 20:16:53 -0800

    chapter 2 drill

---
+++ ch2_helloWorld/hello_world.cpp
@@ -0,0 +1,9 @@
+// Santiago Mondragon
+// Last modified: Feb 11, 2022
+// Summary: Intro Hello World program
+#include <iostream>
+
+int main(void) {
+  std::cout << "Hello World!\n";
+  return 0;
+}
