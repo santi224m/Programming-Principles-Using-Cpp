202aa4bee2238913fe5eb1dc4dd5d2a3f897edf7
Santiago Mondragon <santiago.mondragon224@gmail.com>
2022-02-19 19:15:52 -0800

    change from if statements to switch statement

--- ch4_Computation/measurement_conversion.cpp
+++ ch4_Computation/measurement_conversion.cpp
@@ -1,7 +1,8 @@
 // Santiago Mondragon
 // Last modified: Feb 19, 2022
 // Summay: This program converts from inches to centimeters or centimeters to inches
-// Lesson: Shows how if statements work and how to test for bad input
+// Lesson: Shows how if statements work and how to test for bad input.
+// Also shows how switch statements can be a better option than if statements
 #include <iostream>
 
 int main(void) {
@@ -12,12 +13,24 @@ int main(void) {
   std::cout << "Please enter a length followed by a unit (c or i):\n";
   std::cin >> length >> unit;
 
-  if (unit == 'i')
-    std::cout << length << "in == " << cm_per_inch*length << "cm\n";
-  else if (unit == 'c')
-    std::cout << length << "cm == " << length/cm_per_inch << "in\n";\
-  else
-    std::cout << "Sorry, I don't know a unit called " << unit << "\n";      //an else statement that runs if user enters bad input
+  // if (unit == 'i')
+  //   std::cout << length << "in == " << cm_per_inch*length << "cm\n";
+  // else if (unit == 'c')
+  //   std::cout << length << "cm == " << length/cm_per_inch << "in\n";\
+  // else
+  //   std::cout << "Sorry, I don't know a unit called " << unit << "\n";      //an else statement that runs if user enters bad input
+  
+  switch (unit) {
+    case 'i':
+      std::cout << length << "in == " << cm_per_inch*length << "cm\n";
+      break;
+    case 'c':
+      std::cout << length << "cm == " << length/cm_per_inch << "in\n";\
+      break;
+    default:
+      std::cout << "Sorry, I don't know a unit called " << unit << "\n";      //an else statement that runs if user enters bad input
+      break;
+  }
 
   return 0;
 }
