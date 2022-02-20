d6fbca7a3e83632774336312ecf326dcc0485891
Santiago Mondragon <santiago.mondragon224@gmail.com>
2022-02-19 19:50:11 -0800

    add for loop example

--- ch4_Computation/print_alphabet_values.cpp
+++ ch4_Computation/print_alphabet_values.cpp
@@ -1,7 +1,7 @@
 // Santiago Mondragon
 // Last modified: Feb 19, 2022
 // Summary: This program loops through the characters a-z and prints out their integer value
-// Lesson: Shows how while loops work
+// Lesson: Shows how while loops and for loops work
 #include <iostream>
 
 int main(void) {
@@ -10,6 +10,10 @@ int main(void) {
   while (i < char('z' + 1)) {
     std::cout << char(i) << " " << i << "\n";
     ++i;                                            // increase i value after every iteration
+  }
+
+  for (i = char('A'); i < char('Z' + 1); ++i) {
+    std::cout << char(i) << " " << i << "\n";
   }
 
   return 0;
