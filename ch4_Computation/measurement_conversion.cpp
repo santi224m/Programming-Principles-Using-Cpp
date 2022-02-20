e0a73b9e38fae73ffb570dc8af9adaf9e501b953
Santiago Mondragon <santiago.mondragon224@gmail.com>
2022-02-19 19:06:15 -0800

    create ch4 folder

---
+++ ch4_Computation/measurement_conversion.cpp
@@ -0,0 +1,23 @@
+// Santiago Mondragon
+// Last modified: Feb 19, 2022
+// Summay: This program converts from inches to centimeters or centimeters to inches
+// Lesson: Shows how if statements work and how to test for bad input
+#include <iostream>
+
+int main(void) {
+  constexpr double cm_per_inch = 2.54;    // number of centimeters in an inch
+  double length = 1;                      // length in inches or centimeters
+  char unit = ' ';                         // a space is not a unit
+
+  std::cout << "Please enter a length followed by a unit (c or i):\n";
+  std::cin >> length >> unit;
+
+  if (unit == 'i')
+    std::cout << length << "in == " << cm_per_inch*length << "cm\n";
+  else if (unit == 'c')
+    std::cout << length << "cm == " << length/cm_per_inch << "in\n";\
+  else
+    std::cout << "Sorry, I don't know a unit called " << unit << "\n";      //an else statement that runs if user enters bad input
+
+  return 0;
+}
