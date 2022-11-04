89ac952149076b1b4b3ba443ddef478991178377
Santiago Mondragon <santiago.mondragon@csu.fullerton.edu>
2022-11-04 13:39:12 -0700

    finish Ch 10

--- Ch10_Input_and_Output_Streams/main.cc
+++ Ch10_Input_and_Output_Streams/main.cc
@@ -1,3 +1,4 @@
+#include <fstream>
 #include <ios>
 #include <istream>
 #include <iostream>
@@ -31,6 +32,28 @@ struct Reading {
   int hour;
   double temperature;
 };
+
+std::vector<std::string> month_input_tbl = {
+  "jan", "feb", "mar", "apr", "may", "jun", "jul",
+  "aug", "sep", "oct", "nov", "dec"
+};
+
+int month_to_int(std::string s) {
+  for (int i = 0; i < 12; ++i) {
+    if (month_input_tbl.at(i) == s) return i;
+  }
+  return -1;
+}
+
+std::vector<std::string> month_print_tbl = {
+  "January", "Febuary", "March", "April", "May", "June", "July",
+  "August", "September", "October", "November", "December"
+}
+
+std::string int_to_month(int i) {
+  if (i < 0 || i > 12) throw std::runtime_error("bad month index");
+  return month_print_tbl.at(i);
+}
 
 void end_of_loop(std::istream& ist, char term, const std::string& message) {
   if (ist.fail()) { // use term as terminator
@@ -77,6 +100,9 @@ std::istream& operator>>(std::istream& is, Month& m) {
   // format { month feb ...}
   char ch;
   if (is >> ch && ch != '{') {
+    std::cout << "returning to stream: " << ch << std::endl;
+    int ansi = ch;
+    std::cout << "ansi: " << ansi << std::endl;
     is.unget();
     is.clear(std::ios_base::failbit);
     return is;
@@ -85,6 +111,7 @@ std::istream& operator>>(std::istream& is, Month& m) {
   std::string month_marker;
   std::string mm;
   is >> month_marker >> mm;
+  std::cout << mm << std::endl;
   if (!is || month_marker != "month") {
     throw std::runtime_error("bad start of month");
   }
@@ -101,17 +128,73 @@ std::istream& operator>>(std::istream& is, Month& m) {
     }
     if (invalids) throw std::runtime_error("invalid readings in a month");
     if (duplicates) throw std::runtime_error("duplicate readings in a month");
-    end_of_loop(is, '}', "bad end of month");
+    std::cout << "read a reading\n";
+  }
+  end_of_loop(is, '}', "bad end of month");
+  return is;
+}
+
+std::istream& operator>>(std::istream& is, Year& y) {
+  // read a year from is into y
+  // format: { year 1972 ... }
+  char ch;
+  is >> ch;
+  if (ch != '{') {
+    is.unget();
+    is.clear(std::ios_base::failbit);
     return is;
   }
+
+  std::string year_marker;
+  int yy;
+  is >> year_marker >> yy;
+  std::cout << "year: " << yy << std::endl;
+  if (!is || year_marker != "year") throw std::runtime_error("bad start of year");
+  y.year = yy;
+
+  while (true) {
+    Month m;  // get a clean m each time around
+    std::cout << "created new month object m...\n";
+    if (!(is >> m)) {
+      std::cout << "breaking...\n";
+      break;
+    }
+    y.month[m.month] = m;
+    std::cout << "set value to m: " << m.month << std::endl;
+  }
+  end_of_loop(is, '}', "bad end of year");
+  return is;
 }
 
 int main() {
-  Reading day1;
-  day1.day = 1;
-  day1.hour = 3;
-  day1.temperature =  98.3;
-  std::cout << day1.temperature << std::endl;
+  // Open input file
+  std::cout << "Please enter input file name\n";
+  std::string iname;
+  std::cin >> iname;
+  std::fstream ifs {iname};
+  if (!ifs) throw std::runtime_error("Can't open input file");
+  ifs.exceptions(ifs.exceptions()|std::ios_base::badbit); // throw for bad()
+
+  // Open output file
+  std::cout << "Please enter output file name\n";
+  std::string oname;
+  std::cin >> oname;
+  std::ofstream ofs {oname};
+  if (!ofs) throw std::runtime_error("Can't open output file");
+
+  // read an arbitrary number of years
+  std::vector<Year> ys;
+  while (true) {
+    Year y; // get a freshly initialized Year each time around
+    if (!(ifs >> y)) break;
+    ys.push_back(y);
+  }
+  std::cout << "read " << ys.size() << " years of reading\n";
+
+  // for(Year& y : ys) {
+  //   // print_year(ofs, y);  // function not yet written
+  // }
+
 
   return 0;
 }
